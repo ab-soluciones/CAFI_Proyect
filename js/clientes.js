ce698827d85a1e26386bd5fca5653aaa72d8c7cc
Uriel Cisneros <urielcisne96@gmail.com>
2019-10-15 10:47:18 -0500

    FrontVistas2

--- js/clientes.js
+++ js/clientes.js
@@ -1,113 +1,124 @@
-$(document).ready(function(){
-    //clientes
+$(document).ready(function () {
+    //VUsuarios_ab
     let editar = false;
-    let idclientes = "";
-    obtenerDatosTablaCliente();
-
-    $('.close').click(function(){
-        $('#formclientes').trigger('reset');
+    let idusuario = "";
+    optenerDatosTablaUsuarios();
+  
+    $(".close").click(function () {
+      $("#formulario").trigger("reset");
+      $("#mensaje").css("display", "none");
     });
-
-    function obtenerDatosTablaCliente(){
-        $.ajax({
-            url: 'tablaClientes.php',
-            type: 'GET',
-
-            success: function(response){
-                let datos = JSON.parse(response);
-                let template = '';
-                datos.forEach(datos => {
-                    template+=`
-                    <tr>
-                    <td class="text-nowrap text-center">${datos.id}</td>
-                    <td class="text-nowrap text-center">${datos.nombre}</td>
-                    <td class="text-nowrap text-center">${datos.apt}</td>
-                    <td class="text-nowrap text-center">${datos.apm}</td>
-                    <td class="text-nowrap text-center">${datos.tipodoc}</td>
-                    <td class="text-nowrap text-center">${datos.numdoc}</td>
-                    <td class="text-nowrap text-center">${datos.direccion}</td>
-                    <td class="text-nowrap text-center">${datos.telefono}</td>
-                    <td class="text-nowrap text-center">${datos.correo}</td>
-                    <td class="text-nowrap text-center">${datos.estado}</td>
-                    <th style="width:100px; class="p-1">
-                        <div class="row">
-                            <a  data-toggle="modal" data-target="#modalForm" style="margin: 0 auto;" class="beditar btn btn-danger" href="#">
-                                Editar
-                            </a>
-                        </div>
-                    </th>`;
-                });
-                $('#cuerpo').html(template);
-            }
+  
+    $(".clearForm").click(function () {
+      $("#formuusers").trigger("reset");
+    });
+  
+    $('.agregar').click(function(){
+      editar = false;
+    });
+  
+    $("#login").keyup(function () {
+      var username = $("#login").val();
+      if (username.length >= 3) {
+        $(".contro").show();
+        $.post("username_check.php", {
+          username2: username
+        }, function (data, status) {
+          $("#status").html(data);
         });
+      } else {
+        $(".contro").hide();
+      }
+    });
+  
+    $("#formulario").submit(function (e) {
+      $.post("../Controllers/usuariosab.php",$("#formulario").serialize() + '&accion=' + editar, function (response) {
+        console.log(response);
+        $("#mensaje").css("display", "block");
+        if (response == "Registro exitoso") {
+          $("#mensaje").text(response);
+          $("#mensaje").css("color", "green");
+          $("#email").focus();
+          $("#formulario").trigger("reset");
+        } else {
+          $("#mensaje").text(response);
+          $("#mensaje").css("color", "red");
+          $("#email").focus();
+        }
+        optenerDatosTablaUsuarios();
+      });
+      e.preventDefault();
+    });
+  
+    function optenerDatosTablaUsuarios() {
+      $.ajax({
+        url: "../Controllers/usuariosab.php",
+        type: "POST",
+        data: "tabla=tabla",
+        success: function (response) {
+          
+         let datos = JSON.parse(response);
+          let template = "";
+          $.each(datos, function (i, item) {
+            template += `
+            <tr>
+                  <td class="text-nowrap text-center d-none">${item[0]}</td>
+                  <td class="text-nowrap text-center">${item[1]}</td>
+                  <td class="text-nowrap text-center">${item[2]}</td>
+                  <td class="text-nowrap text-center">${item[3]}</td>
+                  <td class="text-nowrap text-center">${item[4]}</td>
+                  <td class="text-nowrap text-center">${item[5]}</td>
+                  <td class="text-nowrap text-center">${item[6]}</td>
+                  <td class="text-nowrap text-center">${item[7]}</td>
+                  <td class="text-nowrap text-center">${item[8]}</td>
+                  <td class="text-nowrap text-center">${item[9]}</td>
+                  <td class="text-nowrap text-center">${item[10]}</td>
+                  <td class="text-nowrap text-center">${item[11]}</td>
+                  <td class="text-nowrap text-center">${item[12]}</td>
+                  <td class="text-nowrap text-center">${item[13]}</td>
+                  <td class="text-nowrap text-center">${item[14]}</td>
+                  <td class="text-nowrap text-center">${item[15]}</td>
+                  <th class="text-nowrap text-center" style="width:100px;">
+                  <div class="row">
+                      <a data-toggle="modal" data-target="#modalForm" style="margin: 0 auto;" class="BeditarUsuarios btn btn-danger" href="#">
+                        Editar
+                      </a>
+                  </div>
+              </th>
+            `;
+          });
+          $("#cuerpo").html(template); 
+        }
+      });
     }
-
-    $('#bclose').click(function(){
-        
+  
+    $(document).on("click", ".BeditarUsuarios", function () {
+      var valores = "";
+      // Obtenemos todos los valores contenidos en los <td> de la fila
+      // seleccionada
+      $(this).parents("tr").find("td").each(function () {
+        valores += $(this).html() + "?";
+      });
+      datos = valores.split("?");
+      console.log(datos);
+      $("#email").val(datos[0]);
+      $("#rfc").val(datos[1]);
+      $("#nombre").val(datos[2]);
+      $("#cp").val(datos[3]);
+      $("#calle_numero").val(datos[4]);
+      $("#colonia").val(datos[5]);
+      $("#localidad").val(datos[6]);
+      $("#municipio").val(datos[7]);
+      $("#estado").val(datos[8]);
+      $("#pais").val(datos[9]);
+      $("#telefono").val(datos[10]);
+      $("#fecha_nacimiento").val(datos[11]);
+      $("#sexo").val(datos[12]);
+      $("#acceso").val(datos[13]);
+      $("#entrada_sistema").val(datos[14]);
+      $("#contrasena").val(datos[15]);
+      
+      editar = true;
     });
-
-    $('#formclientes').submit(function(e){
-        const postData = {
-            id: idclientes,
-            nombre: $('#nombre').val(),
-            apt: $('#apt').val(),
-            apm: $('#apm').val(),
-            documento: $('#documento').val(),
-            numdoc: $('#numdoc').val(),
-            direccion: $('#dir').val(),
-            telefono: $('#tel').val(),
-            email: $('#email').val(),
-            estado: $('#vcestado').val(),
-        };
-
-        let url = editar === false ? 'post-guardar.php' : 'post-edit.php';
-
-        $.post(url,postData,function(response){
-
-            obtenerDatosTablaCliente();
-            if(response === "1"){
-                swal({
-                    title: 'Exito',
-                    text: 'Datos guardados satisfactoriamente',
-                    type: 'success'
-                },
-                function (isConfirm) {
-                    if (isConfirm) {
-                        console.log("si entro");
-                        $('#formclientes').trigger('reset');
-                        $('.modal').modal('hide');
-                        editar = false;
-                    }
-                  });     
-            }else{
-                swal({
-                    title: 'Alerta',
-                    text: 'Datos no guardados, compruebe los campos unicos',
-                    type: 'warning'
-                });
-            }
-        });
-        e.preventDefault();
-    });
-
-    $(document).on('click','.beditar', function(){
-        var valores = "";
-        $(this).parents("tr").find("td").each(function () {
-            valores += $(this).html() + "?";
-        });
-        datos = valores.split("?");
-
-        idclientes = datos[0];
-        $('#nombre').val(datos[1]);
-        $('#apt').val(datos[2]);
-        $('#apm').val(datos[3]);
-        $('#documento').val(datos[4]);
-        $('#numdoc').val(datos[5]);
-        $('#dir').val(datos[6]);
-        $('#tel').val(datos[7]);
-        $('#email').val(datos[8]);
-        $('#vcestado').val(datos[9]);
-        editar = true;
-    })
-    
-});
+  });
+  
