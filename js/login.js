73251ac48b22785a2ef4f7939619864bce4b5f32
Uriel Cisneros <urielcisne96@gmail.com>
2019-10-17 18:37:45 -0500

    Commit2

--- js/login.js
+++ js/login.js
@@ -11,47 +11,46 @@ $(document).ready(function () {
   });
 
   $("#formulario").submit(function (e) {
-  $.post("../Controllers/login.php",$("#formulario").serialize(), function (response) {
-    console.log(response);
-    $("#mensaje").css("display", "block");
-      if (response != "[]") {
-        let datos = JSON.parse(response);
-        $.each(datos, function (i, item) {
-            if (typeof (item[3]) != 'undefined' ) {
-              if (item[2]=='A') {
-                if (item[1] == 'CEO' && (item)[3] == 'null'){
-                  $('.ocultar').hide();
-                  $('.mostrar').show();
-                    var negocio= $(this).val();
-                    $.post("../Controllers/login.php","combo=combo",function (response) {
-                      let datos =JSON.parse(response);
-                      var template='';
-                      $.each(datos, function(i, item) {
-                        template += `
-                        <option value="${item[0]}">${item[1]}</option>
-                        `;
+    $.post("../Controllers/login.php",$("#formulario").serialize(), function (response) {
+      console.log(response);
+      $("#mensaje").css("display", "block");
+        if (response != "[]") {
+          let datos = JSON.parse(response);
+          $.each(datos, function (i, item) {
+              if (typeof (item[3]) != 'undefined' ) {
+                if (item[2]=='A') {
+                  if (item[1] == 'CEO' && (item)[3] == 'null'){
+                    $('.ocultar').hide();
+                    $('.mostrar').show();
+                      var negocio= $(this).val();
+                      $.post("../Controllers/login.php","combo=combo",function (response) {
+                        let datos =JSON.parse(response);
+                        var template='';
+                        $.each(datos, function(i, item) {
+                          template += `
+                          <option value="${item[0]}">${item[1]}</option>
+                          `;
+                        });
+                        $('#Snegocios').html(template);
                       });
-                      $('#Snegocios').html(template);
-                    });
+                  }else{
+                    console.log('Trabajador');
+                  }
                 }else{
-                  console.log('Trabajador');
+                  $("#mensaje").text("Usuario inactivo");
+                  $("#mensaje").css("color", "red");
                 }
+
               }else{
-                $("#mensaje").text("Usuario inactivo");
-                $("#mensaje").css("color", "red");
+                window.location.replace('usuariosab.html')
+                  console.log('Usuario AB');
               }
-
-            }else{
-              window.location.replace('usuariosab.html')
-                console.log('Usuario AB');
-            }
-        });
-      }else{
-        $("#mensaje").text("Usuario incorrecto");
-        $("#mensaje").css("color", "red");
-      }
-  });
-
-    e.preventDefault();
+          });
+        }else{
+          $("#mensaje").text("Usuario incorrecto");
+          $("#mensaje").css("color", "red");
+        }
     });
+      e.preventDefault();
+      });
   });
